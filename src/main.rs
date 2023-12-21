e3fd6ba49957007590865fa95a7afd296e32a434
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-12-21 10:48:28 -0500

    Create function to take and read user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+struct Task {
+    id: i32,
+    title: String,
+    description: Option<String>,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    //a vector that holds all the tasks
+    let mut vector: Vec<Task> = Vec::new();
+    
+    //take user input
+    fn read_input(prompt: &str) -> String {
+        let mut input = String::new();
+        println!({}, prompt);
+        io::stdin().read_line(&mut input).expect("Unable to read input.");
+        input.trim().to_string()
+    }
+
+    
+    
 }
