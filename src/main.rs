7fab66ad0511c4b673bd0eaccd80f07c7c13bd04
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-12-21 11:32:55 -0500

    Continously ask user for input using a loop and add functionality for viewing all tasks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 struct Task {
-    id: i32,
+    id: usize,
     title: String,
     description: Option<String>,
 }
@@ -9,16 +9,45 @@ struct Task {
 
 fn main() {
     //a vector that holds all the tasks
-    let mut vector: Vec<Task> = Vec::new();
+    let mut tasks: Vec<Task> = Vec::new();
     
     //take user input
     fn read_input(prompt: &str) -> String {
         let mut input = String::new();
-        println!({}, prompt);
+        println!("{}", prompt);
         io::stdin().read_line(&mut input).expect("Unable to read input.");
         input.trim().to_string()
     }
 
-    
+    let new_task = read_input("Welcome to Rusty Task Manager. Please start by adding a task.");
+    let description_input = read_input("Would you like to provide a short description? If not, type N and press Enter. If yes, just type the description.");
+
+    let description = if description_input == "N" { None } else { Some(description_input) };
+
+    tasks.push(Task {
+        id: tasks.len(),
+        title: new_task,
+        description: description
+    });
+
+    println!("Great! Looks like you've successfully added your first task. Let's get you familiarized with the menu. Type 'View' if you'd like to see all your tasks. Type 'Add' if you'd like to add a new task. Type 'update' to update or delete an exisiting task. Type 'Exit' to exit the program.");
+
+    loop {
+        let user_input = read_input("What would you like to do next?");
+        if user_input == "View" {
+            for task in &tasks {
+                println!("Task ID: {} \nTask name: {}", task.id, task.title);
+                match &task.description {
+                    Some(desc) => println!("Description: {}", desc),
+                    None => println!("Description: None")
+                }
+            }
+        }
+        if user_input == "Exit" {
+            return;
+        }
+    }
+
+
     
 }
