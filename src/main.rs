df0ebcc9c8974a52a61e185270b2eb12634212ac
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-12-21 11:53:52 -0500

    Added functionality to add tasks and prevent duplication of code

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ struct Task {
     description: Option<String>,
 }
 
+fn add_task(tasks: &mut Vec<Task>, task_input: String, description_input: String) {
+    let description = if description_input == "N" { None } else { Some(description_input) };
+
+    tasks.push(Task {
+        id: tasks.len(),
+        title: task_input,
+        description: description
+    });
+}
+
 
 fn main() {
     //a vector that holds all the tasks
@@ -19,21 +29,15 @@ fn main() {
         input.trim().to_string()
     }
 
-    let new_task = read_input("Welcome to Rusty Task Manager. Please start by adding a task.");
-    let description_input = read_input("Would you like to provide a short description? If not, type N and press Enter. If yes, just type the description.");
+    let task_input = read_input("\nWelcome to Rusty Task Manager. Please start by adding a task.\n");
+    let description_input = read_input("\nWould you like to provide a short description? If not, type N and press Enter. If yes, just type the description.\n");
+    add_task(&mut tasks, task_input, description_input);
+    
 
-    let description = if description_input == "N" { None } else { Some(description_input) };
-
-    tasks.push(Task {
-        id: tasks.len(),
-        title: new_task,
-        description: description
-    });
-
-    println!("Great! Looks like you've successfully added your first task. Let's get you familiarized with the menu. Type 'View' if you'd like to see all your tasks. Type 'Add' if you'd like to add a new task. Type 'update' to update or delete an exisiting task. Type 'Exit' to exit the program.");
+    println!("\nGreat! Looks like you've successfully added your first task. Let's get you familiarized with the menu. Type 'View' if you'd like to see all your tasks. Type 'Add' if you'd like to add a new task. Type 'update' to update or delete an exisiting task. Type 'Exit' to exit the program.\n");
 
     loop {
-        let user_input = read_input("What would you like to do next?");
+        let user_input = read_input("\nWhat would you like to do next?\n");
         if user_input == "View" {
             for task in &tasks {
                 println!("Task ID: {} \nTask name: {}", task.id, task.title);
@@ -42,8 +46,12 @@ fn main() {
                     None => println!("Description: None")
                 }
             }
-        }
-        if user_input == "Exit" {
+        } else if user_input == "Add" {
+            let task_name = read_input("\nEnter name of task:\n");
+            let desc_input = read_input("\nEnter description or N\n");
+            add_task(&mut tasks, task_name, desc_input);
+
+        } else if user_input == "Exit" {
             return;
         }
     }
